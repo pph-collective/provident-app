97e59725126be521f1fb17215d9e6237e1a2c732
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-03-16 19:04:00 -0400

    refactor: move add admin script to folder

--- add-admin/run.js
+++ add-admin/run.js
@@ -1,5 +1,5 @@
 // env GOOGLE_APPLICATION_CREDENTIALS must contain the path to your firebase credentials
-// usage: `node add_admin.js admin@email.com`
+// usage: `node run.js admin@email.com`
 // the admin must already be a user
 
 var admin = require("firebase-admin");
@@ -11,5 +11,6 @@ app
   .auth()
   .getUserByEmail(email)
   .then(user => {
-    app.auth().setCustomClaims(user.uid, { admin: true });
+    app.auth().setCustomUserClaims(user.uid, { admin: true });
+    process.exit();
   });
