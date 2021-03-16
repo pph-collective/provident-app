e9b7d605293d8835dcfd3e0c05850c0cf340d29d
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-03-16 12:46:46 -0400

    fix: specify region

--- src/firebase.js
+++ src/firebase.js
@@ -22,6 +22,8 @@ if (location.hostname === "localhost") {
   db.useEmulator("localhost", 8088);
   auth.useEmulator("http://localhost:9099");
   fn.useEmulator("localhost", 9090);
+} else {
+  fn = fn.region("us-central1");
 }
 
 export default {
