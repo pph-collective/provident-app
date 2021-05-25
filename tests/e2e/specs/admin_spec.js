362ac285c5c5d8324318a5f51714a1cea11ee732
Ellen Duong <shi.ellenduong@gmail.com>
2021-05-25 15:30:13 -0400

    test: started admin view

--- tests/e2e/specs/admin_spec.js
+++ tests/e2e/specs/admin_spec.js
@@ -1,15 +1,19 @@
-describe("/login", () => {
+describe("Admin", () => {
   beforeEach(() => {
     cy.login("admin@admin.com", "admin1");
+    cy.visit("/admin");
   });
 
-  it("link to /admin", () => {
-    cy.visit("");
+  it("navigation bar to /admin", () => {
     cy.get("#navbar-contents > div.navbar-start > a:nth-child(4)")
       .should("have.attr", "href")
       .and("include", "admin");
   });
 
-  // Navigate to admin tab
+  it("Review Access Requests header", () => {
+    cy.contains("h1", "Review Access Requests");
+  });
+
   // Ability to approve / deny users
+  // TODO: Add users to approve / deny
 });
