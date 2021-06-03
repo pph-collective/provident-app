c7479951f5c2efa20a3835fd53da57684e8db18e
Mary McGrath <m.c.mcgrath13@gmail.com>
2021-06-03 10:25:26 -0400

    feat(dashboard): control panel

---
+++ src/components/dashboard/ControlPanel.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="control-panel box">
+    <div
+      v-for="(options, type) in dropDowns"
+      class="control-panel-dropdown control has-icons-left"
+      :key="'control-panel-dropdown-' + type"
+    >
+      <span class="select">
+        <select
+          @input="$emit('selected', { type, value: $event.target.value })"
+        >
+          <option
+            v-for="(option, index) in options.values"
+            :key="'option-' + index"
+            >{{ option }}</option
+          >
+        </select>
+      </span>
+      <span class="icon is-small is-left pl-1">
+        <i :class="options.icon"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dropDowns: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ["selected"]
+};
+</script>
+
+<style lang="scss" scoped>
+.control-panel {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 1rem;
+  justify-items: center;
+}
+.control-panel-dropdown {
+  width: 100%;
+  max-width: 400px;
+  .select,
+  select {
+    width: 100%;
+  }
+}
+</style>
--- src/views/snack/Dashboard.vue
+++ src/views/snack/Dashboard.vue
@@ -1,9 +1,45 @@
 <template>
-  <p>I am dashboard</p>
+  <div class="dashboard p-4">
+    <ControlPanel :drop-downs="dropDowns" @selected="updateControl($event)" />
+    <p>{{ controls }}</p>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
+import ControlPanel from "@/components/dashboard/ControlPanel.vue";
+
 export default {
-  name: "Dashboard"
+  components: {
+    ControlPanel
+  },
+  setup() {
+    const dropDowns = {
+      geography: {
+        icon: "fas fa-globe",
+        values: ["All of Rhode Island", "RI 4 Good"]
+      },
+      model_version: {
+        icon: "fas fa-calendar-alt",
+        values: ["Current", "2020 Q1"]
+      }
+    };
+
+    const controls = ref({
+      geography: dropDowns.geography.values[0],
+      model_version: dropDowns.model_version.values[0]
+    });
+
+    const updateControl = e => {
+      controls.value[e.type] = e.value;
+    };
+
+    return {
+      dropDowns,
+      controls,
+      updateControl
+    };
+  }
 };
 </script>
