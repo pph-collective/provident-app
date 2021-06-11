857ce3ce367e330737e0a7e0c00ae3d1430d0b75
Ellen Duong <shi.ellenduong@gmail.com>
2021-06-11 09:49:10 -0400

    fix: set form fields on the login page to required, updated tests (#37)

--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -8,34 +8,41 @@ describe("Log In View", () => {
     cy.visit("/login");
   });
 
-  it("greets with Log in", () => {
+  it("Greets with log in", () => {
     cy.contains("h1", "Log In").should("exist");
   });
 
-  it("links to /register", () => {
+  it("Links to /register", () => {
     cy.get("button")
       .contains("Request Access")
       .click();
     cy.url().should("eq", Cypress.config().baseUrl + "register");
   });
 
-  it("requires email", () => {
-    cy.get("form").submit();
-    cy.get('[data-cy="error-message"]').should(
-      "contain",
-      "The email address is badly formatted"
-    );
+  it("Submitting an empty form", () => {
+    cy.get('[data-cy="login-form-button"]').click();
+    // If an exception was thrown here, cypress will fail the test
+
+    // Just make sure we're still not signed in
+    cy.contains("h1", "Log In").should("exist");
+    cy.visit("/");
+    cy.get('[data-cy="navbar-burger"]').click();
+    cy.get('[data-cy="login-button"]').should("exist");
   });
 
-  it("requires password", () => {
-    cy.get('[type="email"]').type(`${accounts.admin.email}{enter}`);
-    cy.get('[data-cy="error-message"]').should(
-      "contain",
-      "The password is invalid or the user does not have a password"
-    );
+  it("Requires a password", () => {
+    cy.get('[type="email"]').type(`${accounts.admin.email}`);
+    cy.get('[data-cy="login-form-button"]').click();
+    // If an exception was thrown here, cypress will fail the test
+
+    // Just make sure we're still not signed in
+    cy.contains("h1", "Log In").should("exist");
+    cy.visit("/");
+    cy.get('[data-cy="navbar-burger"]').click();
+    cy.get('[data-cy="login-button"]').should("exist");
   });
 
-  it("requires valid username and password", () => {
+  it("Requires valid username and password", () => {
     cy.get('[type="email"]').type(accounts.admin.email);
     cy.get('[type="password"]').type("invalid{enter}");
     cy.get('[data-cy="error-message"]').should(
@@ -44,7 +51,7 @@ describe("Log In View", () => {
     );
   });
 
-  it("navigates to / on successful login", () => {
+  it("Navigates to / on successful login", () => {
     cy.get('[type="email"]').type(accounts.admin.email);
     cy.get('[type="password"]').type(`${accounts.admin.password}{enter}`);
     cy.url().should("eq", Cypress.config().baseUrl);
@@ -61,7 +68,7 @@ describe("Log In View", () => {
       .contains("User account not approved: pending");
   });
 
-  it("Log In and Out to check Auth Listener", () => {
+  it("Log in and out to check the auth Listener", () => {
     cy.login_by_permission("admin");
     cy.get('[data-cy="navbar-burger"]').click();
     cy.get('[data-cy="logout-button"]').should("exist");
