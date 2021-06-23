a32106ffe202461c82532a33fe189b11e595e9ac
Ellen Duong <shi.ellenduong@gmail.com>
2021-06-23 17:10:12 -0400

    test: wip - test successful reset password request was made

--- tests/e2e/specs/reset_password.spec.js
+++ tests/e2e/specs/reset_password.spec.js
@@ -8,42 +8,45 @@ describe("Reset Password", () => {
     cy.visit("/login");
   });
 
-  // it("Submit empty email", () => {
-  //   cy.get('[data-cy="reset-password"]').click();
-  //   cy.get('[data-cy="error-message"]').should(
-  //     "contain",
-  //     "Enter an email and then click reset password."
-  //   );
-  // });
-  //
-  // it("Submit not an email", () => {
-  //   cy.get('[type="email"]').type("not an email");
-  //   cy.get('[data-cy="reset-password"]').click();
-  //   cy.get('[data-cy="error-message"]').should(
-  //     "contain",
-  //     "Enter an email and then click reset password."
-  //   );
-  // });
-  //
-  // it("Reset invalid email", () => {
-  //   cy.get('[type="email"]').type("doesnotexist@doesnotexist.com");
-  //   cy.get('[data-cy="reset-password"]').click();
-  //   cy.get('[data-cy="error-message"]').should(
-  //     "contain",
-  //     "There is no user record corresponding to this identifier. The user may have been deleted."
-  //   );
-  // });
+  it("Submit empty email", () => {
+    cy.get('[data-cy="reset-password"]').click();
+    cy.get('[data-cy="error-message"]').should(
+      "contain",
+      "Enter an email and then click reset password."
+    );
+  });
+
+  it("Submit not an email", () => {
+    cy.get('[type="email"]').type("not an email");
+    cy.get('[data-cy="reset-password"]').click();
+    cy.get('[data-cy="error-message"]').should(
+      "contain",
+      "Enter an email and then click reset password."
+    );
+  });
+
+  it("Reset invalid email", () => {
+    cy.get('[type="email"]').type("doesnotexist@doesnotexist.com");
+    cy.get('[data-cy="reset-password"]').click();
+    cy.get('[data-cy="error-message"]').should(
+      "contain",
+      "There is no user record corresponding to this identifier. The user may have been deleted."
+    );
+  });
 
   it("Reset a password for an account that does exist", () => {
-    cy.intercept("POST", "http://localhost:9099/**", req => {
-      if (req.body.requestType === "PASSWORD_RESET") {
-        req.alias = "password-email-reset-request";
+    cy.intercept("POST", "http://localhost:9099/**", {
+      statusCode: 200,
+      body: {
+        requestType: "PASSWORD_RESET",
+        email: `${accounts.approved.email}`
       }
-    });
+    }).as("password-email-reset-request");
 
     cy.get('[type="email"]').type(`${accounts.approved.email}`);
     cy.get('[data-cy="reset-password"]').click();
 
+    // assert that a matching successful password reset request has been made
     cy.wait("@password-email-reset-request").then(req => {
       cy.log(req);
     });
@@ -52,9 +55,5 @@ describe("Reset Password", () => {
       "contain",
       "Success. Check your email to reset your password."
     );
-
-    // Verify the email was sent
-    // Reset the password
-    // Log in with new password
   });
 });
