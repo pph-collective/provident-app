9af7722266c0e791d99cd31e3d4d3349fe1cbb1b
Mary McGrath <m.c.mcgrath13@gmail.com>
2021-07-19 18:09:03 -0400

    build: update sass-loader

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: '@import "@/assets/styles/variables.scss";',
+        additionalData: '@import "@/assets/styles/variables.scss";',
       },
     },
   },
