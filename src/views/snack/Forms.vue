807f997e576a58bc425ba935800c09c8c7c394fa
Ellen Duong <shi.ellenduong@gmail.com>
2021-07-23 12:28:19 -0400

    fix: modal-content --> modal-card
    - now modal header stays on screen and only the body scrolls

--- src/views/snack/Forms.vue
+++ src/views/snack/Forms.vue
@@ -87,7 +87,7 @@
       data-cy="active-form-modal"
     >
       <div class="modal-background"></div>
-      <div class="modal-content is-family-secondary">
+      <div class="modal-card is-family-secondary">
         <header class="modal-card-head">
           <p class="modal-card-title" data-cy="active-form-title">
             {{ activeForm.title }}
@@ -237,7 +237,7 @@ export default {
 }
 
 @include mobile {
-  .modal-content {
+  .modal-card {
     max-height: 100vh;
   }
 
