b86f47684111d4f5b090f1cf230e79241587aa35
Mary McGrath <m.c.mcgrath13@gmail.com>
2021-09-14 11:40:13 -0400

    test: add back config paths

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -125,7 +125,10 @@ module.exports = (on, config) => {
   const extendedConfig = cypressFirebasePlugin(on, config, admin);
 
   return Object.assign({}, extendedConfig, {
+    fixturesFolder: "tests/e2e/fixtures",
     integrationFolder: "tests/e2e/specs",
+    screenshotsFolder: "tests/e2e/screenshots",
+    videosFolder: "tests/e2e/videos",
     supportFile: "tests/e2e/support/index.js",
   });
 };
