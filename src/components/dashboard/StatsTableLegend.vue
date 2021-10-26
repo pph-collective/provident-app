1edc8abceef3d2a7fe0a0c087f0dfab8b749ceb6
Mary McGrath <m.c.mcgrath13@gmail.com>
2021-10-26 11:31:08 -0400

    style: make legend icons also bigger

--- src/components/dashboard/StatsTableLegend.vue
+++ src/components/dashboard/StatsTableLegend.vue
@@ -3,14 +3,21 @@
     <h6 class="is-size-6-7 has-text-weight-bold">Legend:</h6>
     <div
       class="
+        legend-item
         is-size-6-7 is-flex is-flex-wrap-wrap is-justify-content-space-between
       "
     >
-      <div v-for="item in items" :key="item.text">
-        <span class="has-text-weight-bold" :style="{ color: item.color }">{{
-          item.symbol
-        }}</span>
-        {{ item.text }}
+      <div
+        v-for="item in items"
+        :key="item.text"
+        class="is-flex is-align-items-center"
+      >
+        <span
+          class="has-text-weight-bold is-size-5"
+          :style="{ color: item.color }"
+          >{{ item.symbol }}</span
+        >
+        <span class="optical-centering">{{ item.text }}</span>
       </div>
     </div>
   </div>
@@ -48,3 +55,12 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.legend-item {
+  line-height: 1;
+}
+.optical-centering {
+  padding-bottom: 0.12rem;
+}
+</style>
