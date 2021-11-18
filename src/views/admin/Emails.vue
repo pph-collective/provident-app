f4139188fdce62df1d0a01b9a0407dbc5a4c8b5f
Mary McGrath <m.c.mcgrath13@gmail.com>
2021-11-18 16:45:42 -0500

    feat: add logo and link to slack to the footer of emails

--- src/views/admin/Emails.vue
+++ src/views/admin/Emails.vue
@@ -257,7 +257,7 @@ export default {
       try {
         await fb.createEmail({
           subject,
-          body: sanitizeHtml(body),
+          body: processEmailBody(body),
           to,
           sendDate: send_date,
         });
@@ -265,6 +265,13 @@ export default {
       } catch (err) {
         console.log(err);
       }
+    };
+
+    // add the fancier template around the content and sanitize the html
+    const processEmailBody = (body) => {
+      const sanitized = sanitizeHtml(body);
+      const prettyBody = `<table><tr><td>${sanitized}</td></tr><tr><td><div style="max-width: 175px; margin-top: 20px; margin-bottom: 5px;"><img style="width: 100%" src="https://provident.preventoverdoseri.org/assets/images/pori-provident-text-logo.png" alt="PROVIDENT logo"></img></div><a href="https://join.slack.com/t/dataacademytalk/shared_invite/zt-yv25w5bq-d6xR3SCXjjR0XoJi__mdmg">Join our Slack!</a></td></tr></table>`;
+      return prettyBody;
     };
 
     const updatePreview = ({ body, subject }) => {
