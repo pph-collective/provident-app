96da3f131adbd7ec4e0f0985c043d8534dda8dd3
Ellen Duong <shi.ellenduong@gmail.com>
2021-11-19 17:23:21 -0500

    feat: add form for follow up forms and clearer error messages for nested fields

--- scripts/add-form.js
+++ scripts/add-form.js
@@ -50,85 +50,176 @@ const warnAndExit = (warning) => {
 };
 
 const validateForm = (form) => {
+  const warnings = {};
+
   // check title
   if (!form["title"]) {
-    warnAndExit("must provide title");
+    warnings.title = "Required field, a string";
   }
 
   // check type
   const validFormTypes = ["user", "organization"];
   if (!form["type"]) {
-    warnAndExit("must provide a 'type'");
+    warnings.type = "Required field, 'user' or 'organization'";
   } else if (!validFormTypes.includes(form["type"])) {
-    warnAndExit(
-      `invalid form type ${
-        form["type"]
-      }. valid form types: ${validFormTypes.join(", ")}`
-    );
+    warnings.type = `Invalid form type entered: ${form["type"]}. Required field, 'user' or 'organization`;
   }
 
   // check questions
   if (!form["questions"]) {
-    warnAndExit("must provide questions");
+    warnings.questions = "Required field, list of questions";
+  } else {
+    const questionsWarnings = validateQuestions(form["questions"]);
+    if (Object.keys(questionsWarnings).length > 0) {
+      warnings.questions = questionsWarnings;
+    }
   }
 
-  validateQuestions(form["questions"]);
+  if (form["followup_form"]) {
+    const followupFormWarnings = validateFollowupForm(form["followup_form"]);
+
+    if (Object.keys(followupFormWarnings).length > 0) {
+      warnings.followup_form = followupFormWarnings;
+    }
+  }
+
+  if (Object.keys(warnings).length > 0) {
+    warnAndExit(JSON.stringify(warnings, null, 4));
+  }
+};
+
+const validateFollowupForm = (followup_form) => {
+  const warnings = {};
+
+  if (!followup_form["title"]) {
+    warnings.title = "Required field, a string";
+  }
+
+  if (followup_form["type"]) {
+    warnings.type =
+      "Remove field, it gets overwritten to match the original form type";
+  }
+
+  if (!followup_form["questions"]) {
+    warnings.questions = "Required field, list of questions";
+  } else {
+    const questionsWarnings = {};
+
+    Object.entries(followup_form["questions"]).forEach(([key, question]) => {
+      let tempWarnings = {};
+      if (question["source_model"]) {
+        // TODO: source_model exists as model in regular form
+
+        if (!question["label"]) {
+          tempWarnings["label"] = "Required field, a string";
+        }
+
+        if (!question["model"]) {
+          tempWarnings["model"] = "Required field, a string";
+        }
+
+        if (question["component"]) {
+          tempWarnings["component"] =
+            "Invalid field: source_model was provided. We'll pull the component field from the source_model question.";
+        }
+      } else {
+        // Regular question
+        tempWarnings = validateQuestion(question);
+      }
+
+      // Add warnings to
+      if (Object.keys(tempWarnings).length > 0) {
+        questionsWarnings[key] = tempWarnings;
+      }
+    });
+
+    if (Object.keys(questionsWarnings).length > 0) {
+      warnings.questions = questionsWarnings;
+    }
+  }
+
+  return warnings;
+};
+
+const validateQuestion = (question) => {
+  const warnings = {};
+
+  // check label
+  if (!question["label"]) {
+    warnings["label"] = "Required field, a string for the question label";
+  }
+
+  if (question["source_model"]) {
+    warnings["source_model"] =
+      "Invalid field, source_model exists on followup_form questions only";
+  }
+
+  // check component
+  if (!question["component"]) {
+    warnings["component"] = "Required field";
+  } else {
+    switch (question["component"]) {
+      case "Date":
+        ["max_date", "min_date"].forEach((field) => {
+          if (
+            question[field] &&
+            question[field] !== "today" &&
+            !dateRegex.test(question[field])
+          ) {
+            warnings[field] =
+              "Invalid value, should either be 'today' or in yyyy-mm-dd format";
+          }
+        });
+        break;
+      case "TextArea":
+      case "TextInput":
+        break;
+      case "Select":
+      case "Radio":
+      case "Checkbox":
+        if (!question["options"]) {
+          warnings["options"] = "Required field, list of options";
+        }
+        break;
+      case "LikertScale":
+        if (!question["statements"]) {
+          warnings["statements"] = "Required field, list of statements";
+        }
+        break;
+      case "SubForm":
+        if (!question["questions"]) {
+          warnings["questions"] = "Required field, list of questions";
+        } else {
+          const subFormQuestionsWarnings = validateQuestions(
+            question["questions"]
+          );
+          if (Object.keys(subFormQuestionsWarnings).length > 0) {
+            warnings["questions"] = subFormQuestionsWarnings;
+          }
+        }
+        break;
+      default:
+        warnings[
+          "component"
+        ] = `Invalid component, must be of the following: Checkbox, Date, LikertScale, Radio, Select, SubForm, TextArea, TextInput`;
+    }
+  }
+
+  return warnings;
 };
 
 const validateQuestions = (questions) => {
-  Object.entries(questions).forEach(([key, question]) => {
-    // check label
-    if (!question["label"]) {
-      warnAndExit(`question missing label: ${key}`);
-    }
+  const warnings = {};
 
-    // check component
-    if (!question["component"]) {
-      warnAndExit(`question missing component: ${key}`);
-    } else {
-      switch (question["component"]) {
-        case "Date":
-          ["max_date", "min_date"].forEach((field) => {
-            if (
-              question[field] &&
-              question[field] !== "today" &&
-              !dateRegex.test(question[field])
-            ) {
-              warnAndExit(
-                `${field} key should either be 'today' or in yyyy-mm-dd format`
-              );
-            }
-          });
-          break;
-        case "TextArea":
-        case "TextInput":
-          break;
-        case "Select":
-        case "Radio":
-        case "Checkbox":
-          if (!question["options"]) {
-            warnAndExit(`question component requires options: ${key}`);
-          }
-          break;
-        case "LikertScale":
-          if (!question["statements"]) {
-            warnAndExit(`question component requires statements: ${key}`);
-          }
-          break;
-        case "SubForm":
-          if (!question["questions"]) {
-            warnAndExit(`question component requires questions: ${key}`);
-          } else {
-            validateQuestions(question["questions"]);
-          }
-          break;
-        default:
-          warnAndExit(
-            `question has unknown component: ${key} - ${question["component"]} (must be one of TextArea, TextInput, Select, Radio, Checkbox, LikertScale, SubForm)`
-          );
-      }
+  Object.entries(questions).forEach(([key, question]) => {
+    const questionWarnings = validateQuestion(question);
+
+    if (Object.keys(questionWarnings).length > 0) {
+      warnings[key] = questionWarnings;
     }
   });
+
+  return warnings;
 };
 
 // Add the timestamp of this upload as the form version
@@ -161,5 +252,5 @@ db.collection("forms")
     // validate the form and upload
     validateForm(form);
     addVersion(oldForm, form);
-    db.collection("forms").doc(id).set(form);
+    // db.collection("forms").doc(id).set(form);
   });
