028babb487a1b70167a35542ae62ef72da88ef11
Ellen Duong <shi.ellenduong@gmail.com>
2022-01-04 16:15:15 -0500

    refactor: use form type

--- query_examples/specific_form_responses.js
+++ query_examples/specific_form_responses.js
@@ -1,6 +1,6 @@
 // env GOOGLE_APPLICATION_CREDENTIALS must contain the path to your firebase credentials
-// usage: `node user_form_response.js`
-// queries form responses for a specific organization and form type and saves them to a file in downloads
+// usage: `node specific_form_responses.js`
+// queries form responses for a specific user or organization and form type and saves them to a file in downloads
 // called `form_response.json`. The document ID is added to the data's `id` key.
 // adapted from https://firebase.google.com/docs/firestore/query-data/queries
 
@@ -14,11 +14,12 @@ const app = admin.initializeApp();
 const db = app.firestore();
 
 const FILE_PATH = `${os.homedir()}/Downloads/organization_form_responses.json`;
-const ORGANIZATION = "PPH Collective"; // UPDATE THIS!
+const FORM_TYPE = "organization"; // 'user' or 'organization'
+const USER_OR_ORG = "PPH Collective"; // UPDATE THIS!
 const FORM_ID = "neighborhood_rapid_assessment";
 
-db.collection("organization")
-  .doc(ORGANIZATION)
+db.collection(FORM_TYPE)
+  .doc(USER_OR_ORG)
   .collection("form_responses")
   .where("form_id", "==", FORM_ID)
   .get()
