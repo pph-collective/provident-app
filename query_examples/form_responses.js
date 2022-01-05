495a4f890fa20f0429521348fcd4bccadf15b3f1
Ellen Duong <shi.ellenduong@gmail.com>
2022-01-05 08:33:33 -0500

    feat: update the sample queries (#215)
    * feat: update the sample queries
    
    * feat: update file path
    
    * refactor: use form type

--- query_examples/form_responses.js
+++ query_examples/form_responses.js
@@ -6,13 +6,14 @@
 
 const admin = require("firebase-admin");
 const fs = require("fs");
+const os = require("os");
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = "serviceAccount.json";
 
 const app = admin.initializeApp();
 const db = app.firestore();
 
-const FILE_PATH = "~/downloads/form_responses.json";
+const FILE_PATH = `${os.homedir()}/Downloads/form_responses.json`;
 
 db.collectionGroup("form_responses")
   .get()
@@ -22,5 +23,5 @@ db.collectionGroup("form_responses")
       results.push({ id: doc.id, ...doc.data() });
     });
 
-    fs.writeFileSync(FILE_PATH, JSON.stringify(results));
+    fs.writeFileSync(FILE_PATH, JSON.stringify(results, null, 2));
   });
--- query_examples/specific_form_responses.js
+++ query_examples/specific_form_responses.js
@@ -1,23 +1,25 @@
 // env GOOGLE_APPLICATION_CREDENTIALS must contain the path to your firebase credentials
-// usage: `node user_form_response.js`
-// queries form responses for a scpecific user and form type and saves them to a file in downloads
+// usage: `node specific_form_responses.js`
+// queries form responses for a specific user or organization and form type and saves them to a file in downloads
 // called `form_response.json`. The document ID is added to the data's `id` key.
 // adapted from https://firebase.google.com/docs/firestore/query-data/queries
 
 const admin = require("firebase-admin");
 const fs = require("fs");
+const os = require("os");
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = "serviceAccount.json";
 
 const app = admin.initializeApp();
 const db = app.firestore();
 
-const FILE_PATH = "~/downloads/form_response.json";
-const USER = "email@email.com"; // UPDATE THIS!
+const FILE_PATH = `${os.homedir()}/Downloads/organization_form_responses.json`;
+const FORM_TYPE = "organization"; // 'user' or 'organization'
+const USER_OR_ORG = "PPH Collective"; // UPDATE THIS!
 const FORM_ID = "neighborhood_rapid_assessment";
 
-db.collection("user")
-  .doc(USER)
+db.collection(FORM_TYPE)
+  .doc(USER_OR_ORG)
   .collection("form_responses")
   .where("form_id", "==", FORM_ID)
   .get()
@@ -27,5 +29,5 @@ db.collection("user")
       results.push({ id: doc.id, ...doc.data() });
     });
 
-    fs.writeFileSync(FILE_PATH, JSON.stringify(results));
+    fs.writeFileSync(FILE_PATH, JSON.stringify(results, null, 2));
   });
