aa893de5e2f10e531f9efcaf05cea625358b6996
Ellen Duong <shi.ellenduong@gmail.com>
2022-05-25 08:51:27 -0400

    fix: seed script, and wrap logout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -38,7 +38,7 @@ Cypress.Commands.add("login_by_permission", (permission_level) => {
 
 Cypress.Commands.add("logout", () => {
   cy.get(".loading-icon", { timeout: 10000 }).should("not.exist");
-  fb.logout();
+  cy.wrap(fb.logout()).should("eq", undefined);
   cy.get("[data-cy='home']").click();
   cy.get("[data-cy='login-button']").should("exist");
   cy.log("Logged out");
