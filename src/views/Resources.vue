284a4d66187d82ba28cc0340bb15aa1e9a070f93
Ellen Duong <shi.ellenduong@gmail.com>
2023-05-11 13:40:34 -0400

    wip: template

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,38 +1,157 @@
 <template>
-  <div class="dashboard container is-fullhd">
-    <Card id="TODO-0" style="align-self: start" width="full" :height="5">
-      <template #title> TITLE </template>
-      <template #subtitle> SUBTITLE </template>
-      <template #content> CONTENT </template>
-    </Card>
+  <section class="section">
+    <!-- VIDEOS -->
+    <h1 class="title">Section: Videos</h1>
+    <h2 class="subtitle">
+      A simple container to divide your page into <strong>sections</strong>,
+      like the one you're currently reading.
+    </h2>
+    <div class="tile is-ancestor is-vertical">
+      <div class="tile is-parent">
+        <div class="tile is-child box is-4">
+          <p class="title">One</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+        <div class="tile is-child box is-4">
+          <p class="title">Two</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+        <div class="tile is-child box is-4">
+          <p class="title">Three</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+      </div>
+      <div class="tile is-parent">
+        <div class="tile is-child box is-4">
+          <p class="title">Four</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+        <div class="tile is-child box is-4">
+          <p class="title">Five</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+        <div class="tile is-child box is-4">
+          <p class="title">Six</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 
-    <Card id="TODO-1" style="align-self: start" width="two-thirds" :height="5">
-      <template #title> TITLE </template>
-      <template #subtitle> SUBTITLE </template>
-      <template #content> CONTENT </template>
-    </Card>
+  <!-- Downloads -->
+  <section class="section">
+    <h1 class="title">Section: Downloads</h1>
+    <h2 class="subtitle">
+      A simple container to divide your page into <strong>sections</strong>,
+      like the one you're currently reading.
+    </h2>
+    <div class="tile is-ancestor is-vertical">
+      <div class="tile is-parent">
+        <div class="tile is-child box is-4">
+          <p class="title">One</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+        <div class="tile is-child box is-4">
+          <p class="title">Two</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+        <div class="tile is-child box is-4">
+          <p class="title">Three</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+            non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 
-    <Card id="TODO-2" width="one-third" :height="5">
-      <template #title> TITLE 2 </template>
-      <template #subtitle> SUBTITLE </template>
-      <template #content> CONTENT </template>
-    </Card>
+  <!-- Links -->
+  <section class="section">
+    <h1 class="title">Section: Links</h1>
+    <h2 class="subtitle">
+      A simple container to divide your page into <strong>sections</strong>,
+      like the one you're currently reading.
+    </h2>
 
-    <Card id="TODO-3" width="one-third" :height="2" :no-header="true">
-      <!-- This one has no-header set to true in the line above -->
-      <template #content> CONTENT </template>
-    </Card>
+    <div class="card">
+      <div class="card-content">
+        <p class="title">
+          “There are two hard things in computer science: cache invalidation,
+          naming things, and off-by-one errors.”
+        </p>
+        <p class="subtitle">Jeff Atwood</p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            View on
+            <a href="https://twitter.com/codinghorror/status/506010907021828096"
+              >Twitter</a
+            >
+          </span>
+        </p>
+        <p class="card-footer-item">
+          <span> Share on <a href="#">Facebook</a> </span>
+        </p>
+      </footer>
+    </div>
+  </section>
 
-    <Card id="TODO-4" width="one-third" :height="2" :no-header="true">
-      <!-- This one has no-header set to true in the line above -->
-      <template #content> CONTENT </template>
-    </Card>
+  <!-- Glossary -->
+  <section class="section">
+    <h1 class="title">Section: Glossary</h1>
+    <h2 class="subtitle">
+      A simple container to divide your page into <strong>sections</strong>,
+      like the one you're currently reading.
+    </h2>
 
-    <Card id="TODO-5" width="one-third" :height="2" :no-header="true">
-      <!-- This one has no-header set to true in the line above -->
-      <template #content> CONTENT </template>
-    </Card>
-  </div>
+    <div class="container">
+      <div class="card">
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros.
+            Donec id elit non mi porta gravida at eget metus. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Cras mattis consectetur purus sit amet fermentum.
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
