263374afc480bdd61e25b47a872e87bcbdda2c40
Ellen Duong <shi.ellenduong@gmail.com>
2023-05-12 20:48:54 -0400

    wip: resources page

---
+++ src/components/ExternalLinkBig.vue
@@ -0,0 +1,42 @@
+<template>
+  <a :href="href" target="_blank" rel="noopener noreferrer">
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ title }}</p>
+            <p class="subtitle is-6">{{ subtitle }}</p>
+          </div>
+          <div class="media-right">
+            <span class="icon is-large">
+              <span class="fa-stack fa-lg">
+                <i class="fas fa-external-link-alt"></i>
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+    },
+    subtitle: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/main.scss";
+</style>
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -215,31 +215,95 @@
       A simple container to divide your page into <strong>sections</strong>,
       like the one you're currently reading.
     </h2>
-    <div class="tile is-ancestor is-vertical">
-      <div class="tile is-parent">
-        <div class="tile is-child box is-4">
-          <p class="title">One</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
-            non massa sem. Etiam finibus odio quis feugiat facilisis.
-          </p>
+
+    <div class="columns is-multiline">
+      <div class="column is-one-third">
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img src="https://bulma.io/images/placeholders/256x256.png" />
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">File Name</p>
+                <p class="subtitle is-6">May 12, 2023</p>
+              </div>
+            </div>
+
+            <div class="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+              <a href="#">#responsive</a>
+              <br />
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">Download</a>
+            </footer>
+          </div>
         </div>
-        <div class="tile is-child box is-4">
-          <p class="title">Two</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
-            non massa sem. Etiam finibus odio quis feugiat facilisis.
-          </p>
+      </div>
+
+      <div class="column is-one-third">
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img src="https://bulma.io/images/placeholders/256x256.png" />
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">File Name</p>
+                <p class="subtitle is-6">May 12, 2023</p>
+              </div>
+            </div>
+
+            <div class="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+              <a href="#">#responsive</a>
+              <br />
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">Download</a>
+            </footer>
+          </div>
         </div>
-        <div class="tile is-child box is-4">
-          <p class="title">Three</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-            ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
-            non massa sem. Etiam finibus odio quis feugiat facilisis.
-          </p>
+      </div>
+
+      <div class="column is-one-third">
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img src="https://bulma.io/images/placeholders/256x256.png" />
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">File Name</p>
+                <p class="subtitle is-6">May 12, 2023</p>
+              </div>
+            </div>
+
+            <div class="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+              <a href="#">#responsive</a>
+              <br />
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">Download</a>
+            </footer>
+          </div>
         </div>
       </div>
     </div>
@@ -253,27 +317,54 @@
       like the one you're currently reading.
     </h2>
 
-    <div class="card">
-      <div class="card-content">
-        <p class="title">
-          “There are two hard things in computer science: cache invalidation,
-          naming things, and off-by-one errors.”
-        </p>
-        <p class="subtitle">Jeff Atwood</p>
+    <div class="columns is-multiline">
+      <div class="column is-one-third">
+        <ExternalLinkBig
+          href="https://www.google.com/maps"
+          title="Google Maps"
+          subtitle="Geography woo~"
+        />
       </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            View on
-            <a href="https://twitter.com/codinghorror/status/506010907021828096"
-              >Twitter</a
-            >
-          </span>
-        </p>
-        <p class="card-footer-item">
-          <span> Share on <a href="#">Facebook</a> </span>
-        </p>
-      </footer>
+
+      <div class="column is-one-third">
+        <ExternalLinkBig
+          href="https://www.google.com/maps"
+          title="Google Maps"
+          subtitle="Geography woo~"
+        />
+      </div>
+
+      <div class="column is-one-third">
+        <ExternalLinkBig
+          href="https://www.google.com/maps"
+          title="Google Maps"
+          subtitle="Geography woo~"
+        />
+      </div>
+
+      <div class="column is-one-third">
+        <ExternalLinkBig
+          href="https://www.google.com/maps"
+          title="Google Maps"
+          subtitle="Geography woo~"
+        />
+      </div>
+
+      <div class="column is-one-third">
+        <ExternalLinkBig
+          href="https://www.google.com/maps"
+          title="Google Maps"
+          subtitle="Geography woo~"
+        />
+      </div>
+
+      <div class="column is-one-third">
+        <ExternalLinkBig
+          href="https://www.google.com/maps"
+          title="Google Maps"
+          subtitle="Geography woo~"
+        />
+      </div>
     </div>
   </section>
 
@@ -301,7 +392,7 @@
 </template>
 
 <script setup>
-import Card from "@/components/dashboard/Card.vue";
+import ExternalLinkBig from "@/components/ExternalLinkBig.vue";
 </script>
 
 <style lang="scss" scoped>
