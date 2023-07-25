0b4f4a94189ffb1fc04bacb851698c0d0ed9c58a
Ellen Duong <shi.ellenduong@gmail.com>
2023-07-25 16:17:26 -0400

    feat: copy script with command line arguments

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,6 +1,32 @@
+const { ArgumentParser } = require("argparse");
 const admin = require("firebase-admin");
 
-const emulator = false;
+const parser = new ArgumentParser({
+  description: "PROVIDENT - copy data",
+  add_help: true,
+});
+
+parser.add_argument("-e", "--emulator", {
+  action: "store_true",
+  help: "use emulator",
+});
+
+parser.add_argument("-c", "--collection", {
+  required: true,
+  help: "collection name",
+});
+
+parser.add_argument("-f", "--from", {
+  required: true,
+  help: "from document name",
+});
+
+parser.add_argument("-t", "--to", {
+  required: true,
+  help: "to document name",
+});
+
+const { emulator, collection, from, to } = parser.parse_args();
 
 if (emulator) {
   console.log("using emulator");
@@ -11,8 +37,8 @@ process.env.GOOGLE_APPLICATION_CREDENTIALS = "serviceAccount.json";
 const app = admin.initializeApp();
 const db = app.firestore();
 
-const copyFrom = db.collection("svi_data").doc("2022-1");
-const copyTo = db.collection("svi_data").doc("2022-2");
+const copyFrom = db.collection(collection).doc(from);
+const copyTo = db.collection(collection).doc(to);
 
 copyFrom.get().then((value) => {
   copyTo.set(value.data());
