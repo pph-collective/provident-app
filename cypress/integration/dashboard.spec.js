0da5618d3bdda711ded3ad4323dd297f0fb44501
Anna Murphy <anna_murphy1@brown.edu>
2023-08-10 16:15:33 -0400

    update slice

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -652,10 +652,11 @@ describe("Dashboard viewed as an admin", () => {
       .should("have.length", 43)
       .then((options) => {
         const actual = [...options].map((o) => o.text);
-        expect(actual.slice(0, 3)).to.deep.eq([
+        expect(actual.slice(0, 4)).to.deep.eq([
           "All of Rhode Island",
           "Good Doers",
           "Other",
+          "RI 4 Us",
         ]);
       });
   });
