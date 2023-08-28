9f4a2497a36177a6b0a8ad845510d1e5bb5399bd
Anna Murphy <anna_murphy1@brown.edu>
2023-08-28 15:14:07 -0400

    fix: removed comments

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const logger = require("firebase-functions/logger");
 const functions = require("firebase-functions");
 const firestore = require("@google-cloud/firestore");
@@ -37,11 +28,3 @@ exports.scheduledFirestoreExport = functions.pubsub
 
     logger.info("Ending Backup Function");
   });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
