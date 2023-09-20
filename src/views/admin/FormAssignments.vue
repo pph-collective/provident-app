5210cc8a0eed2e0c743cf3e6b8f9852e29fb5df3
Ellen Duong <shi.ellenduong@gmail.com>
2023-09-20 10:02:09 -0400

    refactor: esc --> vEsc, EmailsPage, FormAssignments, OrganizationManagement, RegisterUser

--- src/views/admin/FormAssignments.vue
+++ src/views/admin/FormAssignments.vue
@@ -133,13 +133,13 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { computed, ref } from "vue";
 import { useStore } from "vuex";
 
 import { db, addFormAssignment, createEmail } from "@/firebase";
 import { doc, deleteDoc } from "firebase/firestore";
-import { esc } from "@/directives/escape";
+import { esc as vEsc } from "@/directives/escape";
 import utils from "@/utils/utils";
 import formAssignmentUtils from "@/utils/formAssignment";
 
@@ -147,246 +147,215 @@ import JSONForm from "@/components/form/JSONForm.vue";
 import LoadingSpinner from "@/components/LoadingSpinner.vue";
 import PanelTag from "@/components/PanelTag.vue";
 
-export default {
-  components: {
-    JSONForm,
-    LoadingSpinner,
-    PanelTag,
-  },
-  directives: {
-    ...esc,
-  },
-  setup() {
-    const closeFormRequest = ref(0);
-    const formMessage = ref("");
-    const showModal = ref(false);
+const closeFormRequest = ref(0);
+const formMessage = ref("");
+const showModal = ref(false);
 
-    const store = useStore();
-    const forms = computed(() => store.state.forms);
-    const organizations = computed(() => store.state.organizations);
-    const users = computed(() => store.getters.approvedUsers);
-    const formAssignments = computed(() => store.state.formAssignments);
-    const pageLoading = computed(
-      () => users.value.length === 0 || forms.value.length === 0,
+const store = useStore();
+const forms = computed(() => store.state.forms);
+const organizations = computed(() => store.state.organizations);
+const users = computed(() => store.getters.approvedUsers);
+const formAssignments = computed(() => store.state.formAssignments);
+const pageLoading = computed(
+  () => users.value.length === 0 || forms.value.length === 0,
+);
+const formLoading = ref(false);
+
+const today = utils.today();
+
+const tabs = {
+  "Active (Not Expired)": (formAssignment) =>
+    today <= formAssignment.expire_date,
+  Released: (formAssignment) =>
+    formAssignment.release_date <= today && today <= formAssignment.expire_date,
+  Expired: (formAssignment) => today > formAssignment.expire_date,
+  All: () => true,
+};
+const selectedTab = ref(Object.keys(tabs)[0]);
+const selectedFormAssignments = computed(() =>
+  formAssignments.value.filter(tabs[selectedTab.value]),
+);
+
+const formQuestions = computed(() => {
+  if (
+    Object.keys(forms.value).length === 0 ||
+    users.value.length === 0 ||
+    organizations.value.length === 0
+  ) {
+    return [];
+  }
+
+  const formOptions = Object.values(forms.value)
+    .map((f) => {
+      return { value: f, label: `${f.title} (type: ${f.type})` };
+    })
+    .sort(utils.sortByProperty("label"));
+
+  const userOptions = store.getters.formUserOptions;
+  const organizationOptions = store.getters.formOrganizationOptions;
+  const groups = formAssignmentUtils.TARGET_GROUPS;
+
+  return [
+    {
+      component: "Select",
+      label: "Form",
+      model: "form",
+      options: formOptions,
+      required: true,
+    },
+    {
+      component: "Select",
+      multiple: true,
+      label: "Assign to groups",
+      model: "target_groups",
+      options: groups,
+    },
+    {
+      component: "Select",
+      multiple: true,
+      label: "Assign to organizations",
+      model: "target_organizations",
+      options: organizationOptions,
+    },
+    {
+      component: "Select",
+      multiple: true,
+      label: "Assign to users",
+      help_text: "Only user forms can be directly assigned to users.",
+      model: "target_users",
+      default: [],
+      options: userOptions,
+      condition: "(model) => model.form && model.form.type === 'user'",
+    },
+    {
+      component: "Date",
+      label: "Release Date",
+      help_text: "The date when the form will be released to users.",
+      model: "release_date",
+      required: true,
+      min_date: "today",
+    },
+    {
+      component: "Date",
+      label: "Expire Date",
+      help_text:
+        "The due date of the form and the expire date of the form assignment. The form won't be assigned to anyone new after this date.",
+      model: "expire_date",
+      required: true,
+      min_date: "today",
+    },
+    {
+      component: "Checkbox",
+      label: "Send Email?",
+      help_text:
+        "Send an email to targeted users on the release date notifying them of this form.",
+      model: "send_email",
+      default: [],
+      options: ["Yes, send a notification"],
+    },
+  ];
+});
+
+const createFormAssignment = async ({
+  form,
+  release_date,
+  expire_date,
+  target_users = [],
+  target_organizations = [],
+  target_groups = [],
+  send_email,
+}) => {
+  formLoading.value = true;
+
+  const { type, title } = form;
+
+  const target = {
+    users: target_users,
+    organizations: target_organizations,
+    groups: target_groups,
+  };
+
+  const formAssignmentData = {
+    created_date: Date.now(),
+    form,
+    release_date,
+    expire_date,
+    target,
+  };
+
+  let emails = [];
+  try {
+    // Create the form assignment on the db
+    formAssignmentData._id = await addFormAssignment(formAssignmentData);
+
+    emails = await formAssignmentUtils.addFormResponses(
+      formAssignmentData,
+      organizations.value,
+      users.value,
     );
-    const formLoading = ref(false);
 
-    const today = utils.today();
+    // Update the page
+    store.dispatch("addFormAssignment", formAssignmentData);
 
-    const tabs = {
-      "Active (Not Expired)": (formAssignment) =>
-        today <= formAssignment.expire_date,
-      Released: (formAssignment) =>
-        formAssignment.release_date <= today &&
-        today <= formAssignment.expire_date,
-      Expired: (formAssignment) => today > formAssignment.expire_date,
-      All: () => true,
-    };
-    const selectedTab = ref(Object.keys(tabs)[0]);
-    const selectedFormAssignments = computed(() =>
-      formAssignments.value.filter(tabs[selectedTab.value]),
-    );
+    // assigned form to self
+    if (emails.includes(store.state.user.data.email)) {
+      await store.dispatch("updateUserFormResponses");
+    }
 
-    const formQuestions = computed(() => {
-      if (
-        Object.keys(forms.value).length === 0 ||
-        users.value.length === 0 ||
-        organizations.value.length === 0
-      ) {
-        return [];
-      }
+    showModal.value = false;
 
-      const formOptions = Object.values(forms.value)
-        .map((f) => {
-          return { value: f, label: `${f.title} (type: ${f.type})` };
-        })
-        .sort(utils.sortByProperty("label"));
-
-      const userOptions = store.getters.formUserOptions;
-      const organizationOptions = store.getters.formOrganizationOptions;
-      const groups = formAssignmentUtils.TARGET_GROUPS;
-
-      return [
-        {
-          component: "Select",
-          label: "Form",
-          model: "form",
-          options: formOptions,
-          required: true,
-        },
-        {
-          component: "Select",
-          multiple: true,
-          label: "Assign to groups",
-          model: "target_groups",
-          options: groups,
-        },
-        {
-          component: "Select",
-          multiple: true,
-          label: "Assign to organizations",
-          model: "target_organizations",
-          options: organizationOptions,
-        },
-        {
-          component: "Select",
-          multiple: true,
-          label: "Assign to users",
-          help_text: "Only user forms can be directly assigned to users.",
-          model: "target_users",
-          default: [],
-          options: userOptions,
-          condition: "(model) => model.form && model.form.type === 'user'",
-        },
-        {
-          component: "Date",
-          label: "Release Date",
-          help_text: "The date when the form will be released to users.",
-          model: "release_date",
-          required: true,
-          min_date: "today",
-        },
-        {
-          component: "Date",
-          label: "Expire Date",
-          help_text:
-            "The due date of the form and the expire date of the form assignment. The form won't be assigned to anyone new after this date.",
-          model: "expire_date",
-          required: true,
-          min_date: "today",
-        },
-        {
-          component: "Checkbox",
-          label: "Send Email?",
-          help_text:
-            "Send an email to targeted users on the release date notifying them of this form.",
-          model: "send_email",
-          default: [],
-          options: ["Yes, send a notification"],
-        },
-      ];
+    store.dispatch("addNotification", {
+      color: "success",
+      message: `Form assignment added: ${title}`,
     });
 
-    const createFormAssignment = async ({
-      form,
-      release_date,
-      expire_date,
-      target_users = [],
-      target_organizations = [],
-      target_groups = [],
-      send_email,
-    }) => {
-      formLoading.value = true;
+    // add an email to the queue
+    if (send_email.length > 0) {
+      const body = `<p>A form, <em>${title}</em>, has been assigned to ${
+        type === "user" ? "you" : "your organization"
+      }. Check out the form on <a href='${
+        location.origin
+      }/snack/forms'>PROVIDENT</a>.</p>`;
 
-      const { type, title } = form;
+      await createEmail({
+        subject: `PROVIDENT New Form: ${title}`,
+        body,
+        to: emails,
+        sendDate: release_date,
+      });
+    }
+  } catch (err) {
+    console.log(err);
 
-      const target = {
-        users: target_users,
-        organizations: target_organizations,
-        groups: target_groups,
-      };
+    if (formAssignmentData._id) {
+      await deleteDoc(doc(db, "form_assignments", formAssignmentData._id));
+    }
 
-      const formAssignmentData = {
-        created_date: Date.now(),
-        form,
-        release_date,
-        expire_date,
-        target,
-      };
+    // only show the form error if the error was with form assignment, not email
+    if (showModal.value) {
+      formMessage.value = "Error creating form responses for form assignments.";
+    } else {
+      store.dispatch("addNotification", {
+        color: "danger",
+        message: "Form assignment successfully saved. Error creating email.",
+      });
+    }
+  }
 
-      let emails = [];
-      try {
-        // Create the form assignment on the db
-        formAssignmentData._id = await addFormAssignment(formAssignmentData);
+  formLoading.value = false;
+};
 
-        emails = await formAssignmentUtils.addFormResponses(
-          formAssignmentData,
-          organizations.value,
-          users.value,
-        );
-
-        // Update the page
-        store.dispatch("addFormAssignment", formAssignmentData);
-
-        // assigned form to self
-        if (emails.includes(store.state.user.data.email)) {
-          await store.dispatch("updateUserFormResponses");
-        }
-
-        showModal.value = false;
-
-        store.dispatch("addNotification", {
-          color: "success",
-          message: `Form assignment added: ${title}`,
-        });
-
-        // add an email to the queue
-        if (send_email.length > 0) {
-          const body = `<p>A form, <em>${title}</em>, has been assigned to ${
-            type === "user" ? "you" : "your organization"
-          }. Check out the form on <a href='${
-            location.origin
-          }/snack/forms'>PROVIDENT</a>.</p>`;
-
-          await createEmail({
-            subject: `PROVIDENT New Form: ${title}`,
-            body,
-            to: emails,
-            sendDate: release_date,
-          });
-        }
-      } catch (err) {
-        console.log(err);
-
-        if (formAssignmentData._id) {
-          await deleteDoc(doc(db, "form_assignments", formAssignmentData._id));
-        }
-
-        // only show the form error if the error was with form assignment, not email
-        if (showModal.value) {
-          formMessage.value =
-            "Error creating form responses for form assignments.";
-        } else {
-          store.dispatch("addNotification", {
-            color: "danger",
-            message:
-              "Form assignment successfully saved. Error creating email.",
-          });
-        }
+const nonEmptyVals = (obj) => {
+  const res = {};
+  if (obj) {
+    for (const [k, v] of Object.entries(obj)) {
+      if (v.length > 0) {
+        res[k] = v;
       }
+    }
+  }
 
-      formLoading.value = false;
-    };
-
-    const nonEmptyVals = (obj) => {
-      const res = {};
-      if (obj) {
-        for (const [k, v] of Object.entries(obj)) {
-          if (v.length > 0) {
-            res[k] = v;
-          }
-        }
-      }
-
-      return res;
-    };
-
-    return {
-      closeFormRequest,
-      createFormAssignment,
-      formAssignments,
-      formMessage,
-      formQuestions,
-      forms,
-      formLoading,
-      pageLoading,
-      nonEmptyVals,
-      selectedFormAssignments,
-      selectedTab,
-      showModal,
-      tabs,
-    };
-  },
+  return res;
 };
 </script>
 
