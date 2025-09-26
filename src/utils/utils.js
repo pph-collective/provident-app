458e76493e80724e5eea5788a30ee9db947745f0
Ellen Duong <ellen_duong1@brown.edu>
2025-09-26 12:06:16 -0400

    refactor: cleanup (#360)
    * refactor: remove form config
    
    * refactor: remove form utils
    
    * refactor: remove more utils
    
    * refactor: remove notifications
    
    * refactor: clean up

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,9 +4,6 @@ export const MUNICIPALITIES = [
   ...new Set(GEO.map((g) => g.properties.name)),
 ].sort();
 
-export const GEOID_QUESTION_MODEL = "bg_id";
-export const MUNI_QUESTION_MODEL = "municipality";
-
 export const sortByProperty = (property) => (a, b) => {
   let valA = a[property];
   let valB = b[property];
@@ -28,15 +25,6 @@ export const sortByProperty = (property) => (a, b) => {
   return 0;
 };
 
-export const today = () => {
-  return new Date().toLocaleDateString("sv");
-};
-
-// Returns date in "yyyy-mm-dd" format in local timezone
-export const toISODateString = (dateNumber) => {
-  return new Date(dateNumber).toLocaleDateString("sv");
-};
-
 export const uniqueId = () => {
   const dateString = Date.now().toString(36);
   const randomness = Math.random().toString(36).substr(2);
@@ -55,64 +43,12 @@ export const priorityToColor = {
   "Lower risk for overdose": "#DBD4D000", // Transparent
 };
 
-export const uniqueArray = (array) => {
-  const set = new Set(array);
-  return Array.from(set).sort();
-};
-
 export const poriRed = "#990000";
 
-export const cloneDeep = (value) => JSON.parse(JSON.stringify(value));
-
-export const evalSchema = (s, yup) => {
-  s.forEach((q) => {
-    for (const key in q) {
-      if (["condition", "validations", "read_only"].includes(key)) {
-        q[key] = eval(q[key]);
-      } else if (key === "component" && !q[key].startsWith("Form")) {
-        q[key] = "Form" + q[key];
-      }
-    }
-  });
-
-  return yup;
-};
-
-export const FORM_CONFIG = [
-  {
-    shortTitle: "Assessment",
-    title: "Neighborhood Rapid Assessment",
-    iconClass: "fas fa-clipboard",
-  },
-  {
-    shortTitle: "Plan",
-    title: "Six Month Resource Plan",
-    iconClass: "fas fa-tasks",
-  },
-  {
-    shortTitle: "Mid-way Plan Followup",
-    title: "Mid-way Followup to the Six Month Resource Plan",
-    iconClass: "fas fa-star-half",
-  },
-  {
-    shortTitle: "Plan Followup",
-    title: "Followup to Six Month Resource Plan",
-    iconClass: "fas fa-star",
-  },
-];
-
 export default {
-  GEOID_QUESTION_MODEL,
   MUNICIPALITIES,
-  MUNI_QUESTION_MODEL,
   poriRed,
   tertileColorMap,
-  cloneDeep,
-  evalSchema,
   sortByProperty,
-  today,
-  toISODateString,
-  uniqueArray,
   uniqueId,
-  FORM_CONFIG,
 };
